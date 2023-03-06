ec0094df7d2c2edb47831db4c09eb5fb39ee1dc0
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-06 15:04:35 +0600

    route setup done

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -2,22 +2,28 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+
+//express app
 const app = express();
 
 //middlewares
 app.use(cors());
-
 app.use(express.json()); //req for post req
 
-//port
+// endpoints
+app.use("/api/user", userRoutes);
 
+//port
 const PORT = process.env.PORT || 4000;
 
 //mongodb connect
-
 mongoose
-  .connect(process.env.MONGO_KEY)
+  .connect(process.env.MONGO_KEY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     //listening for request
     app.listen(PORT, () => {
